46467922ec1412e27e7b653af0cdf47ef717b0ec
Josh Nelson <joshcdnelson@gmail.com>
2018-07-05 16:14:50 -0700

    Created api POST endpoint for list creation

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     resources :boards, only: [:index, :show, :create]
+    resources :lists, only: [:create]
   end
 
   get '/boards/:id', to: 'home#index'
