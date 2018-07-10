17b2ad93a32c93d8c5b59d4356309a5d39706e38
Ryann McQuilton <rmcquilton@gmail.com>
2018-07-10 12:22:02 -0700

    Close modal on click

--- app/javascript/components/card/Card.jsx
+++ app/javascript/components/card/Card.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import CardHeader from './CardHeader'
 import CardMain from './CardMain'
 import CardButtons from './CardButtons'
 
 class Card extends React.Component {
+
   render() {
     return (
       <div id="modal">
-        <i className="x-icon icon close-modal"></i>
+        <Link to={`/boards/${this.props.card.board_id}`}>
+          <i className="x-icon icon close-modal"></i>
+        </Link>
         <CardHeader
           title={this.props.card.title}
           listTitle={this.props.listTitle}
