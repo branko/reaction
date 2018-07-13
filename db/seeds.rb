caec43c415579a46c3ddee02a1e6012c4e94b112
Ryann McQuilton <rmcquilton@gmail.com>
2018-07-13 15:39:40 -0700

    New seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ board_1 = Board.create(title: 'Meals')
 list_1 = List.create(title: 'Breakfast', board: board_1)
 list_2 = List.create(title: 'Lunch', board: board_1)
 
-card_1 = list_1.cards.create(title: 'Oatmeal', description: 'Maple and brown sugar', board: board_1, labels: ['red'], due_date: '2018-07-04')
+card_1 = list_1.cards.create(title: 'Oatmeal', description: 'Maple and brown sugar', board: board_1, labels: ['red'], due_date: '2018-07-04', completed: true)
 card_2 = list_1.cards.create(title: 'Cereal', board: board_1, labels: [], due_date: '2018-09-05')
 card_3 = list_2.cards.create(title: 'Pad Thai', description: 'Extra peanuts', board: board_1, labels: [])
 card_4 = list_2.cards.create(title: 'BLT', board: board_1, labels: ['yellow', 'green', 'blue'], due_date: '2018-11-02')
